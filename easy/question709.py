e859b12010ca2255d48ecb2c3be109ac89a7013b
modih1 <modih1@mcmaster.ca>
2019-02-23 13:26:12 -0500

    add question 709

---
+++ easy/question709.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        output = []
+        for i in range(len(str)):
+            if 65<=ord(str[i]) <= 90:
+                output.append(chr(ord(str[i]) + 32))
+            else:
+                output.append(str[i])
+        
+        return "".join(output)
+        
