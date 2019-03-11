99bcc8380dfa28411c57d4640f17aab663fd8948
modih1 <modih1@mcmaster.ca>
2019-03-11 01:39:01 -0400

    modify question 17

--- medium/question17.py
+++ medium/question17.py
@@ -11,4 +11,33 @@ class Solution(object):
 
         
         
+#backtracking
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        map = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+               '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+
+        def back_tracking(com, digit):
+            if not digit:
+                output.append(com)
+            else:
+                
+                for letter in map[digit[0]]:
+                    back_tracking(com+letter,digit[1:])
+        
+        
+        
+        output = []
+        if digits:
+            back_tracking("", digits)
+            
+        return output
+        
+
+        
+        
         
