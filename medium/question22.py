b9808572b2e51e16e943bc689f7448f67868ac75
modih1 <modih1@mcmaster.ca>
2019-03-12 01:00:00 -0400

    update question 22

--- medium/question22.py
+++ medium/question22.py
@@ -17,5 +17,32 @@ class Solution(object):
             return self.generate(cur + "(", openP - 1, 1)
         else:
             return self.generate(cur + '(', openP - 1, closeP + 1) + self.generate(cur + ')', openP, closeP - 1)
+
+# different solution
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        
+        def generate(string, n,openP):
+            
+            if n == 0:
+                res.append(string + ")"* openP)
+            elif openP == 0:
+                generate(string + "(", n-1, 1)
+            else:
+                generate(string + "(", n -1, openP + 1)
+                generate(string + ")", n, openP-1)  
+        
+        
+        
+        
+        res = []
+        generate("", n,0)
+        return res
+        
         
         
