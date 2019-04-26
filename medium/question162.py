1a719435b381f4780c8186ea9e5e47452061f401
modih1 <modih1@mcmaster.ca>
2019-04-26 01:42:29 -0400

    add question 162

---
+++ medium/question162.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l,r = 0, len(nums) -1    
+        
+        #binary search for peak
+        while (l < r):            
+            mid = (l + r)/2            
+            if (nums[mid] < nums[mid+1]):
+                l = mid + 1
+            else:
+                r = mid
+                
+        return l
+            
+        
