cba2acafc76d2fab577fcb2288aa74c8f98ddfd2
modih1 <modih1@mcmaster.ca>
2019-04-26 00:12:29 -0400

    add faster solution

--- medium/question139.py
+++ medium/question139.py
@@ -16,3 +16,20 @@ class Solution(object):
           
         return seg[-1]
     
+# using functional programming ot shorten the length of inner loop
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        seg = [False] * len(s) + [False]
+        seg[0] = True
+        
+        for i in range(1,len(s)+1):
+            
+            seg[i] = any( seg[j] and s[j:i] in wordDict for j in range(i))   
+          
+        return seg[-1]
+    
