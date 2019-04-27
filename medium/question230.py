257e0c7c3a76ffd40eb9e7e9fe81bec98d71cf6d
modih1 <modih1@mcmaster.ca>
2019-04-27 01:14:01 -0400

    add faster solution

--- medium/question230.py
+++ medium/question230.py
@@ -27,4 +27,45 @@ class Solution(object):
             self.val = root.val
         self.k -= 1
         self.small(root.right)
+
+# faster solution
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        # keep track of stack
+        stack = []
+        stack.append(root)
+        while root:
+            stack.append(root)
+            root = root.left
+        
+        #Inorder traversal
+        while stack:
+            node = stack.pop()
+            x = node.right
+            while x:
+                stack.append(x)
+                x = x.left
+            k -= 1
+            
+            if k == 0:
+                return node.val
+        
+        return None
+            
+            
+        
+        
         
