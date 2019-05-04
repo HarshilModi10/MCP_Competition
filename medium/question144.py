f9c589573b189179677156fec02d026e19021b88
modih1 <modih1@mcmaster.ca>
2019-05-04 14:17:39 -0400

    add question 144 iterative solution

--- medium/question144.py
+++ medium/question144.py
@@ -28,5 +28,41 @@ class Solution(object):
         self.preorder_traversal(root.left)
         self.preorder_traversal(root.right)
         
+#terative solution
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        
+        #keep track of order
+        stack = []
+        #return list
+        res = []
+        stack.append(root)
+        
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+                
+        return res
+        
+        
+        
+        
         
         
