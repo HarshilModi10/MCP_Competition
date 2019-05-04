c9bcca61ab001d2e29980ca690e86ab061d779c4
modih1 <modih1@mcmaster.ca>
2019-05-04 18:10:44 -0400

    add question 78 faster solution

--- medium/question78.py
+++ medium/question78.py
@@ -15,6 +15,26 @@ class Solution(object):
         res.append(path)        
         for i in range (index, len(nums)):
             self.dfs(res,nums,path + [nums[i]], i + 1)
+
+#another solution
+
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if not nums:
+            return []
+        subsets = [[]]
+        
+        for num in nums:
+            for i in range(len(subsets)):
+                subsets.append(subsets[i] + [num])            
+                
+        return subsets
+        
+        
         
         
         
