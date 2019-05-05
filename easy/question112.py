3ecb9ab8a5bd6372bc98e43d929095071e9d76a2
modih1 <modih1@mcmaster.ca>
2019-05-05 15:39:00 -0400

    add question 112

---
+++ easy/question112.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        
+        return self.helper_path_sum(root, sum)
+    
+    def helper_path_sum(self, node, sum):
+        
+        if not node:
+            return False
+        
+        if not node.left and not node.right:
+            return sum == node.val
+
+        
+        else:
+            return self.helper_path_sum(node.left, sum - node.val) or self.helper_path_sum(node.right, sum - node.val)
+        return False
+        
+        
