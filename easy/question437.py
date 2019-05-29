2ff7484f27e9a04089807aab4220b78e08c985cf
modih1 <modih1@mcmaster.ca>
2019-05-28 20:31:49 -0400

    faster solution

--- easy/question437.py
+++ easy/question437.py
@@ -43,3 +43,54 @@ class Solution(object):
         self.getTotal (root.left, sum - root.val)
         self.getTotal(root.right, sum - root.val)
         
+# faster solution
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        if not root:
+            return 0
+        
+        self.count = 0
+        self.dic = {0:1}
+        self.helperPathSum(root, sum, 0)
+    
+        return self.count 
+    
+    def helperPathSum(self, root, actual_sum, current_sum):
+        if not root:
+            return 
+        
+        current_sum += root.val
+        target = current_sum - actual_sum
+        #check if a path can be formed with previous elements
+        if target in self.dic:
+            self.count += self.dic[target]
+        
+        #add current sum to dictionary
+        if current_sum in self.dic:
+            self.dic[current_sum] += 1
+        else:
+            self.dic[current_sum] = 1
+            
+        self.helperPathSum(root.left, actual_sum, current_sum)
+        self.helperPathSum(root.right, actual_sum, current_sum)
+        
+        #remove path after being done recursively
+        self.dic[current_sum] -= 1
+        
+        
+        
+        
+        
