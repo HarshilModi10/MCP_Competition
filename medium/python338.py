b584772902151f9bab806e06485a5cbf3894d9b6
modih1 <modih1@mcmaster.ca>
2019-06-01 13:23:44 -0400

    add question 338

--- medium/python338.py
+++ medium/python338.py
@@ -1,5 +1,5 @@
 
-# time O(N) * integer size and space: O(N)
+# time O(N) * integer size and space: O(N) array
 class Solution(object):
     def countBits(self, num):
         """
@@ -25,3 +25,48 @@ class Solution(object):
             num >>= 1
         
         return count
+
+
+#Faster solution using dynamic programming space O(N) time O(N)
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        """
+        how does number of 1s work
+        0 -> 0
+        1 -> 1
+        2 -> 10:1
+        3 -> 11:2
+        4 -> 100:1
+        5 -> 101:2
+        6: -> 110: 2
+        7: -> 111: 3
+        
+        
+        100100 -> 100011
+        
+        """
+        if num == 0:
+            return [0]
+        
+        bit = 1
+        n_bit = bit << 1
+        output = [0]
+        
+        for i in range(1,num+1):
+            if n_bit <= i:
+                bit = n_bit
+                n_bit *= 2
+                
+            count = 1
+            rem = i % bit
+            count += output[rem]
+            output.append(count)
+            
+        return output
+            
+            
+        
