e464f2f79a5437c85e29600824b49e89d0131393
modih1 <modih1@mcmaster.ca>
2019-06-02 20:12:52 -0400

    add faster solution

--- medium/question102.py
+++ medium/question102.py
@@ -70,3 +70,37 @@ class Solution(object):
                 
         return (res)
        
+    #faster solution 
+
+    # Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        levels = []
+        queue = []
+        queue.append(root)
+        
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            levels.append(level)
+        
+        return levels
+        
