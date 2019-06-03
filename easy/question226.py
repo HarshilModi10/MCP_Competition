8535ade2b7499dd3c4ce411d9d5ae145af9a5e3c
modih1 <modih1@mcmaster.ca>
2019-06-02 20:12:39 -0400

    add faster solution

--- easy/question226.py
+++ easy/question226.py
@@ -21,3 +21,35 @@ class Solution(object):
         
         return root
         
+
+#iterative solution
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        
+        if not root:
+            return 
+        stack = []
+        stack.append(root)
+        
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        
+        return root
+        
