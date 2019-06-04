da302e316cadf22b4a0c2151ad5c9e7e5682a931
modih1 <modih1@mcmaster.ca>
2019-06-03 21:59:51 -0400

    faster solution

--- easy/question235.py
+++ easy/question235.py
@@ -21,4 +21,30 @@ class Solution(object):
             return self.lowestCommonAncestor(root.right, p, q)
             
         
+#iterative solution
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        while True:
+            if p.val <= root.val <= q.val or q.val <= root.val <= p.val:
+                return root
+            elif p.val < root.val:
+                root = root.left
+            else:
+                root = root.right
+            
+        
         
