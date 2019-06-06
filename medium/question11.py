147cd59e8874e651e330861a25f18a725461c7f9
modih1 <modih1@mcmaster.ca>
2019-06-05 22:25:47 -0400

    add another solution

--- medium/question11.py
+++ medium/question11.py
@@ -20,4 +20,26 @@ class Solution(object):
                 end -= 1
                 
         return globalMax
+
+#another solution 
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        
+        volume = 0
+        left = 0
+        right = len(height) - 1
+        
+        while left < right:
+            volume = max(volume, (right - left) * min(height[left], height[right]))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+                
+        return volume
         
