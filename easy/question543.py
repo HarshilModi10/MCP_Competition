065c0c9b0e4207b15ac8d9aa04000f612c064deb
modih1 <modih1@mcmaster.ca>
2019-06-08 23:53:41 -0400

    faster solution

--- easy/question543.py
+++ easy/question543.py
@@ -28,4 +28,35 @@ class Solution(object):
         
         self.max = max(self.max,L+R+1)
         return max(L,R) + 1
+    
+#faster solution
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        
+        self.max = 0
+        self.getMax(root)
+        return self.max
+    
+    def getMax(self, root):
+        if not root:
+            return 0
+        left = self.getMax(root.left) + 1
+        right = self.getMax(root.right) + 1
+        
+        self.max = max(self.max, left+right -2)
+        return max(left, right)
         
