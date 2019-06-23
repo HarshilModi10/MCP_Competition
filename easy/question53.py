e30cf57c0f65a85ee8ac9ae17652999a94d659b8
modih1 <modih1@mcmaster.ca>
2019-06-23 00:14:04 -0400

    Faster solution

--- easy/question53.py
+++ easy/question53.py
@@ -14,4 +14,19 @@ class Solution(object):
         
         return global_max
         
+#Faster solution
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        local_max, global_max = -float("inf"), -float("inf")
+        
+        for num in nums:
+            local_max = max(local_max + num, num)
+            global_max = max(global_max, local_max)
+        
+        return global_max
         
