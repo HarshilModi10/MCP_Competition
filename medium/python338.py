5d5b8f950be356827c34f767813ba88bf9b3e884
modih1 <modih1@mcmaster.ca>
2019-06-23 00:51:42 -0400

    Faster solution

--- medium/python338.py
+++ medium/python338.py
@@ -68,5 +68,33 @@ class Solution(object):
             
         return output
             
+    #Faster solution
+
+    class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        if num == 0:
+            return [0]
+        if num == 1:
+            return [0,1]
+        if num == 2:
+            return [0,1,1
+                   ]
+        dp = [0] * (num + 1)
+        dp[1] = 1
+        dp[2] = 1
+        bit = 1
+        
+        for i in range(3, num+1):
+            if i > 2**(bit+1):
+                bit += 1
+                
+            dp[i] = 1 + dp[i % 2**bit]
+        
+        return dp[:num+1]
+        
             
         
