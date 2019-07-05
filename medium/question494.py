c95a992f1cb43224735ca05e29de7733a0470133
modih1 <modih1@mcmaster.ca>
2019-07-04 23:08:34 -0400

    add solutions to question 495

--- medium/question494.py
+++ medium/question494.py
@@ -15,4 +15,29 @@ class Solution(object):
         else:
             return self.count_solutions(nums, S, index + 1, total + nums[index]) + self.count_solutions(nums, S, index +1, total - nums[index])
         
+
+#memoization solution
+
+class Solution(object):
+    def findTargetSumWays(self, nums, S):
+        """
+        :type nums: List[int]
+        :type S: int
+        :rtype: int
+        """
+        cache = {}
+        return self.count_solutions(nums, S, 0, 0, cache)
+    
+    def count_solutions(self, nums, S, index, total, cache):
+        if (index, total) in cache:
+            return cache[(index, total)]
+        else:
+            if index == len(nums):
+                if total == S:
+                    return 1
+                return 0
+            else:
+                cache[(index, total)] = self.count_solutions(nums, S, index + 1, total + nums[index], cache) + self.count_solutions(nums, S, index +1, total - nums[index], cache)
+                return cache[(index, total)]
+        
         
