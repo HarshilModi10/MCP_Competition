e1efd3835446b2de88cd8f81f6f1c6498f83bd88
modih1 <modih1@mcmaster.ca>
2019-07-06 22:19:55 -0400

    add solutions to question 518

--- medium/question518.py
+++ medium/question518.py
@@ -64,3 +64,23 @@ class Solution(object):
                 dp[i][j] += dp[i-1][j]
                 
         return dp[-1][-1]
+
+    #better memory
+
+    class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        dp = [0] * (amount+1)
+        
+        dp[0] = 1
+        
+        for c in coins:
+            for j in range(1, amount+1):
+                if c <= j:
+                    dp[j] += dp[j - c]
+    
+        return dp[-1]
