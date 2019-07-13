37fe8990d42d0752a01a99f4df79d660fae0ed6a
modih1 <modih1@mcmaster.ca>
2019-07-13 18:36:16 -0400

    add solutions to question 238

--- medium/question238.py
+++ medium/question238.py
@@ -13,4 +13,23 @@ class Solution(object):
             output[i-1] *= total
         
         return output
+
+# Faster solution
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        output = [1] * len(nums)
+        
+        for i in range(1,len(nums)):
+            output[i] *= output[i-1] * nums[i-1]
+        
+        for i in range(len(nums)-2,-1, -1):
+            output[i] *= nums[i+1]
+            nums[i] *= nums[i+1]
+        
+        return output
             
