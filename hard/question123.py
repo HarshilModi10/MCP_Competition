1b889b18ab9b16f67b3d730a757fb1e4fef0889f
modih1 <modih1@mcmaster.ca>
2019-07-13 22:30:35 -0400

    add solutions to question 123

---
+++ hard/question123.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        
+        even_profits = [0 for i in range(len(prices))]
+        odd_profits = [0 for i in range(len(prices))]
+        
+        for i in range(1,3):
+            memo = [-float("inf")] * len(prices)
+            if i%2 == 0:
+                current_profits = even_profits
+                previous_profits = odd_profits
+            else:
+                current_profits = odd_profits
+                previous_profits = even_profits
+            for j in range(1,len(prices)):
+                memo[j] = max(memo[j-1], -prices[j-1] + previous_profits[j-1])
+                current_profits[j] = max(current_profits[j-1], memo[j] + prices[j])
+
+        return max(even_profits[-1], odd_profits[-1])
+        
+        
