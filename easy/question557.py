e15bd5fda26900eb74ec0b138a38112b9d9f7c33
modih1 <modih1@mcmaster.ca>
2019-07-19 23:00:33 -0400

    add solutions to question 557

--- easy/question557.py
+++ easy/question557.py
@@ -12,3 +12,23 @@ class Solution(object):
         
         return " ".join(output)
         
+
+# faster solution
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return s
+        
+        words = s.split(" ")
+        
+        for i, word in enumerate(words):
+            words[i] = word[::-1]
+        
+        return " ".join(words)
+    
+            
