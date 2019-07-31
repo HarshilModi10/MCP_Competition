fe1f0026cbed3aa7bdd3ae4bb93f307a6718f90e
modih1 <modih1@mcmaster.ca>
2019-07-30 20:53:57 -0400

    add solutions to question 173

--- medium/question173.py
+++ medium/question173.py
@@ -49,3 +49,55 @@ class BSTIterator(object):
 # obj = BSTIterator(root)
 # param_1 = obj.next()
 # param_2 = obj.hasNext()
+
+#faster solution
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class BSTIterator(object):
+
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.stack = list()
+        while root:
+            self.stack.append(root)
+            root = root.left
+        
+
+    def next(self):
+        """
+        @return the next smallest number
+        :rtype: int
+        """
+        node = self.stack.pop()
+        if node.right:
+            x = node.right
+            while x:
+                self.stack.append(x)
+                x = x.left
+                
+        return node.val
+        
+
+    def hasNext(self):
+        """
+        @return whether we have a next smallest number
+        :rtype: bool
+        """
+        if self.stack:
+            return True
+        return False
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
