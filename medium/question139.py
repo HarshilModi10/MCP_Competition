79862e84217a8625760d081dbe86584488c41b98
modih1 <modih1@mcmaster.ca>
2019-08-01 21:36:15 -0400

    add solutions to question 139

--- medium/question139.py
+++ medium/question139.py
@@ -33,3 +33,42 @@ class Solution(object):
           
         return seg[-1]
     
+
+#faster solution
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        cache = {}
+        if not s:
+            return True
+        
+        if not wordDict:
+            return False
+
+        word_dict = set(wordDict)
+        return self.word_break(word_dict, s, 0, cache)
+    
+    def word_break(self, word_dict, s, index, cache):
+        if not s:
+            return True
+        
+        if index == len(s):
+            return False
+        
+        if s[:index+1] in cache:
+            return cache[s[:index+1]]
+        
+        if s[:index+1] in word_dict:
+            temp = self.word_break(word_dict, s[index+1:], 0, cache) or self.word_break(word_dict, s, index+1, cache)
+            cache[s[:index+1]] = temp
+            return temp
+        else:
+            temp = self.word_break(word_dict, s, index+1, cache)
+        
+        cache[s[:index+1]] = temp
+        return temp
+        
